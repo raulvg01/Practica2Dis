994324bb87103360b20a0a635159e1a84b2b1953
Yago Baamonde <9005709@ALUMNOS.UFV.ES>
2023-06-29 11:13:50 +0200

    Descarga SpringBoot

---
+++ Practica2BackEnd/src/main/java/com/UFV/prExt/PrExtApplication.java
@@ -0,0 +1,13 @@
+package com.UFV.prExt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrExtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrExtApplication.class, args);
+	}
+
+}
---
+++ Practica2BackEnd/src/test/java/com/UFV/prExt/PrExtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UFV.prExt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrExtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
