2b748d52e2025ed476c8a882346d31b6210f8359
MarcosMela <9005699@alumnos.ufv.es>
2023-06-30 14:26:40 +0200

    Ajustada las propiedades del proyecto de acuerdo al enunciado y finalización de los métodos básicos del back-end

--- Practica2BackEnd/src/main/java/com/UFV/prExt/DataWrapper.java
+++ Practica2BackEnd/src/main/java/com/UFV/prExt/DataWrapper.java
@@ -4,14 +4,25 @@ package com.UFV.prExt;
 import java.util.List;
 
 public class DataWrapper {
-    private List<TIA_ZonasBasicas> data;
+    private List<TIA_ZonasBasicas> dataZonasBasicas;
+    private List<TIA_ZonasBasicas_Edad> dataZonasEdad;
 
-    public List<TIA_ZonasBasicas> getData() {
-        return data;
+
+    public List<TIA_ZonasBasicas> getDataBasicas() {
+        return dataZonasBasicas;
     }
 
-    public void setData(List<TIA_ZonasBasicas> data) {
-        this.data = data;
+    public void setDataBasicas(List<TIA_ZonasBasicas> dataZonasBasicas) {
+        this.dataZonasBasicas = dataZonasBasicas;
     }
+
+    public List<TIA_ZonasBasicas_Edad> getDataEdad() {
+        return dataZonasEdad;
+    }
+
+    public void setDataEdad(List<TIA_ZonasBasicas_Edad> dataZonasEdad) {
+        this.dataZonasEdad = dataZonasEdad;
+    }
+
 }
 
